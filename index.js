4383d71b935fcb720215b4af3184cbc18cca194d
mani1911 <mail2aj19@gmail.com>
2022-05-26 21:25:16 +0530

    created a working random tile generator

--- index.js
+++ index.js
@@ -1,49 +1,76 @@
 const container = document.getElementsByClassName('container');
 const tiles = document.querySelectorAll('.tile')
 
-let isQuestionOver = false;
-let gameOver = false;
 const tileArray = [];
+var remainingTiles = [];
+var tileMap = {};
 
-
-const selectRandomTile = ()=>{
-    let tileNum = Math.floor(Math.random()*16);
-    while(true){
-        if(tileArray.includes(tileNum)){
-            tileNum = Math.floor(Math.random()*16);
-        }
-        else{
-            return tileNum;
-        }
+const restartGame = ()=>{
+    for(let i=0;i<16;i++){
+        remainingTiles.push(i);
     }
+    tileMap = {};
+    tileMap[0] = [];
 };
 
-const displayTile = (n)=>{
-     const blink = setTimeout(()=>{
-        tiles[n].classList.toggle('dummy')
-    },500);
-    setTimeout(()=>{
-        tiles[n].classList.toggle('dummy')
-    },750);
-}; 
+const selectRandomTile = ()=>{
+    let size = remainingTiles.length;
+    if(size==0) return -1;
+    let tilePosition = Math.floor(Math.random()*size);
+    let tileNum = remainingTiles[tilePosition];
+    remainingTiles = remainingTiles.filter(i => i!= tileNum)
+    return tileNum;
+};
 
-const playQuestion = (n)=>{
-    const question = setInterval(()=>{
-        let randTile = selectRandomTile();
-        tileArray.push(randTile);
-        displayTile(randTile);
-    },1500);
-    setTimeout(()=>{clearInterval(question)}, n*1500);
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const displayTile = async (n)=>{
+    tiles[n].classList.toggle('dummy');
+    await timeout(1000);
+    tiles[n].classList.toggle('dummy');
+}; 
 
-tiles.forEach(tile => {
+const printOrder = async (tilesList)=>{
+    for(let element of tilesList){
+        await timeout(1000);
+        displayTile(element);
+    };
+}; 
+
+const playQuestion = async (n)=>{
+    let prevOrder = tileMap[n-1];
+    let newTile = selectRandomTile();
+    let currentOrder = prevOrder.slice();
+    currentOrder.push(newTile);
+    tileMap[n] = currentOrder;
+    await printOrder(tileMap[n]);
+};
+
+const startGame = async ()=>{
+    restartGame();
+    await playQuestion(1);
+    
+    await timeout(1000);
+    await playQuestion(2);
+    await timeout(1000);
+    await playQuestion(3);
+    await timeout(1000);
+    await playQuestion(4);
+    
+}
+
+startGame();
+
+
+
+
+/*tiles.forEach(tile => {
     tile.addEventListener('click', (e)=>{
         console.log(e.target.innerHTML);
     })
-});
-
-playQuestion(3);
+}); */
 
 
 

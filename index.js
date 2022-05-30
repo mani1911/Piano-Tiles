f9f14e8dfc215cebe91e01b26c653c6184245fdc
mani1911 <mail2aj19@gmail.com>
2022-05-30 10:17:08 +0530

    added hacker mode ui and logic

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const container = document.getElementsByClassName('container');
-const tiles = document.querySelectorAll('.tile')
-const button = document.querySelector('button');
+const normalCont = document.querySelector('.normal-container');
+const hackerCont = document.querySelector('.hacker-container');
+const normalBtn = document.querySelector('.normal');
+const hackerBtn = document.querySelector('.hacker');
+const tiles = document.querySelectorAll('.tile');
+const tiles1 = document.querySelectorAll('.tile1');
+
 const score = document.querySelector('.score');
 const audio = new Audio('click.wav');
 const roundProgressAudio = new Audio('round-progress.wav');
 const error = new Audio('error.wav');
-const tileArray = [];
-
+var isNormalMode = null;
 var isGameOver = true;
 var remainingTiles = [];
 var tileMap = {};
 var userInput = [];
 var gameOver = false;
 var renderingRound = false;
-const resetGame = ()=>{
-    for(let i=0;i<16;i++){
+var playerLost = false;
+
+
+const setMode = async (mode)=>{
+    isNormalMode = mode;
+    await timeout(10);
+    
+}
+
+const resetGame = ()=>{   
+    remainingTiles=[];
+    for(let i=0;i<(isNormalMode?16:36);i++){
         remainingTiles.push(i);
     }
+    isNormalMode?renderClick(tiles):renderClick(tiles1);
     tileMap = {};
     tileMap[0] = [];
     gameOver = false;
@@ -28,16 +42,22 @@ const updateScore = (points)=>{
     score.innerHTML = `Score : ${points}`;
 }
 
-const renderClick = ()=>{
-    tiles.forEach(tile=>{
+
+const renderClick = (t)=>{
+
+    t.forEach(tile=>{
         tile.addEventListener("click", ()=>{
-            userInput.push(parseInt(tile.innerHTML));
+            if(!userInput.includes(parseInt(tile.innerHTML))){
+                userInput.push(parseInt(tile.innerHTML));
+            }
+            
         })
     })
 }
 
 
 const selectRandomTile = ()=>{
+    console.log(remainingTiles)
     let size = remainingTiles.length;
     if(size==0) return -1;
     let tilePosition = Math.floor(Math.random()*size);
@@ -51,11 +71,12 @@ function timeout(ms) {
 }
 
 const displayTile = async (n)=>{
+    let t = isNormalMode?tiles:tiles1;
     audio.play();
-    tiles[n].classList.toggle('dummy');
+    t[n].classList.toggle('dummy');
     await timeout(500);
-    tiles[n].classList.toggle('dummy');
-    audio.stop()
+    t[n].classList.toggle('dummy');
+    
 }; 
 
 const printOrder = async (tilesList)=>{
@@ -78,18 +99,12 @@ const readQuestion = async (n)=>{
     tileMap[n] = currentOrder;
 };
 
-const checkInput = ()=>{
-    console.log('Hello')
-}
 
-const setUserInput = async ()=>{
-    tiles.foreach
-}
 
 const readInput = async (n)=>{
     userInput = [];
     while(userInput.length < n){
-        await timeout(100);
+        await timeout(10);
     };
     return userInput;
 }
@@ -101,25 +116,49 @@ const playRound = async (n)=>{
 }
 
 const normalEvaluate = (n)=>{
+    let checkArray = userInput.slice();
     if(tileMap[n].length != userInput.length){
         return false;
     }
-    for(let i=0;i<tileMap[n].length; i++){
-        if(tileMap[n][i] != userInput[i]){
-            return false;
+    if(isNormalMode){
+        for(let i=0;i<tileMap[n].length; i++){
+            if(checkArray.includes(tileMap[n][i])){
+                checkArray = checkArray.filter(num => num != tileMap[n][i])
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    else{
+        for(let i=0; i<tileMap[n].length; i++){
+            if(checkArray[i] != tileMap[n][i]){
+                return false;
+            }
         }
     }
     return true;
 }
 
-renderClick();
+
+const credits = async(p)=>{
+    await timeout(1500);
+    if(playerLost){
+        alert(`Game Over - Your Score : ${p}`)
+    }
+    else{
+        alert(`You Won the Round - Your Score : ${p}`)
+    }
+    
+}
 
 const startGame = async ()=>{
     let points = 1;
-    while(points <= 16 && !gameOver){
+    while(points <= (isNormalMode?16:36) && !gameOver){
         await playRound(points);
         gameOver = !normalEvaluate(points);
         if(gameOver) {
+            playerLost = true;
             await error.play();
             break;
         };
@@ -127,10 +166,28 @@ const startGame = async ()=>{
         updateScore(points-1);
         await roundProgressAudio.play();
     }
+    await credits(points-1);
 }
 
 
-button.addEventListener("click", ()=>{
+normalBtn.addEventListener("click", ()=>{
+    alert('Order of Tiles is not Mandatory')
+    setMode(true);
+    tiles1.forEach(tile=>{
+        tile.removeEventListener("click", ()=>{
+            userInput.push(parseInt(tile.innerHTML));
+        })
+    })
     resetGame();
+    normalCont.style.display = "flex";
+    hackerCont.style.display = "none";
+    startGame();
+})
+hackerBtn.addEventListener("click", ()=>{
+    alert('Order of tiles is Mandatory')
+    setMode(false);
+    resetGame();
+    normalCont.style.display = "none";
+    hackerCont.style.display = "flex";
     startGame();
 })
